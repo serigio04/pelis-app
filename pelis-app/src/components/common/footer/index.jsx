28cf9de0e284debc36e5ef69837a80e14169b558
serigio04 <serigi.gomar15@gmail.com>
2024-11-24 13:17:35 -0600

    [ADD] agregando estilos al footer

--- pelis-app/src/components/common/footer/index.jsx
+++ pelis-app/src/components/common/footer/index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import './index.scss'
 
 const Footer = () => {
     return (
         <>
-            <footer className="footer-container">
+            <footer className="footer">
                 <p>Derechos reservados</p>
             </footer>
         </>
