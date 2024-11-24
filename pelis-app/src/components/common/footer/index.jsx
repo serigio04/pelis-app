8623c9a2368edf4424ba85544c20e0c1975a8a33
serigio04 <serigi.gomar15@gmail.com>
2024-11-24 16:30:45 -0600

    [UPDATE] actualizacion del contenido del footer

--- pelis-app/src/components/common/footer/index.jsx
+++ pelis-app/src/components/common/footer/index.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 import './index.scss'
+import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
         <>
             <footer className="footer">
-                <p>Derechos reservados</p>
+                <div className="footer-dev">
+                    <p>
+                        Desarrollado por 
+                        <span> Sergio Gomar </span>
+                    </p>   
+                </div>
+                <ul className="footer-media">
+                    <li className="footer-media-links">
+                        Whatsapp
+                    </li>
+                    <li className="footer-media-links">
+                        Email
+                    </li>
+                    <li className="footer-media-links">
+                        Linkedin
+                    </li>
+                </ul>
             </footer>
         </>
     )
