67914a71c63558b6154e89c5ada68dc201c11a32
serigio04 <serigi.gomar15@gmail.com>
2024-11-27 22:48:42 -0600

    [ADD] se añadio la pagina principal

---
+++ pelis-app/src/components/homepage/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import './index.scss';
+
+const HomePage = () => {
+    <>
+        <div className="welcome">
+            <img src="" alt="Logo" />
+            <p className="welcome-text">
+                Bienvenido a la plataforma para ver películas
+            </p>
+            <button className="welcome-button">
+                Ver películas
+            </button>
+        </div>
+    </>
+}
+
+export default HomePage;
